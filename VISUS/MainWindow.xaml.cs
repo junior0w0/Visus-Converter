deecf095afe1a9f4dd4334a98f957bc6878221f2
junior0w0 <92807269+junior0w0@users.noreply.github.com>
2022-11-26 16:29:24 +0700

    Add files via upload

---
+++ VISUS/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UTS_Kelompok3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClickLogin(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Login_Page();
+        }
+
+        private void BtnClickSignup(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new SignUp_page();
+        }
+    }
+}
